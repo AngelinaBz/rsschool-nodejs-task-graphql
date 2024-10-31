2c752a5ec068fc76ece1f06d418d7b01d800bdf4
Angelina Bezik <angelina1337bz@gmail.com>
2024-10-31 23:20:21 +0300

    feat: add input types

---
+++ src/routes/graphql/types/input.ts
@@ -0,0 +1,56 @@
+import { GraphQLNonNull, GraphQLString, GraphQLInputObjectType, GraphQLBoolean, GraphQLInt, GraphQLFloat } from 'graphql';
+import { UUIDType } from './uuid.js';
+import { MemberTypeId } from './membertype.js';
+
+export const ChangePostInput = new GraphQLInputObjectType({
+    name: 'ChangePostInput',
+    fields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+    },
+});
+
+export const ChangeProfileInput = new GraphQLInputObjectType({
+    name: 'ChangeProfileInput',
+    fields: {
+        isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+        yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+        memberTypeId: { type: new GraphQLNonNull(MemberTypeId) },
+    },
+});
+
+export const ChangeUserInput = new GraphQLInputObjectType({
+    name: 'ChangeUserInput',
+    fields: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        balance: { type: new GraphQLNonNull(GraphQLFloat) },
+        memberTypeId: { type: new GraphQLNonNull(MemberTypeId) },
+    },
+});
+
+export const CreatePostInput = new GraphQLInputObjectType({
+    name: 'CreatePostInput',
+    fields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(UUIDType) },
+    },
+});
+
+export const CreateProfileInput = new GraphQLInputObjectType({
+    name: 'CreateProfileInput',
+    fields: {
+        isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+        yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+        userId: { type: new GraphQLNonNull(UUIDType) },
+        memberTypeId: { type: new GraphQLNonNull(MemberTypeId) },
+    },
+});
+
+export const CreateUserInput = new GraphQLInputObjectType({
+    name: 'CreateUserInput',
+    fields: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    },
+});
